cf9c20805395b04e6b4d01f0b2cfa0752a639e0a
rindish krishna <rindishkrishna@gmail.com>
2022-01-31 23:07:20 +0530

    Basic UI implemented

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="inner-container">
+        <div className="heading">Todomatic</div>
+        <div>
+        <textarea className="text" >
+       
+        </textarea>
+        </div>
+        <button className="btn">Add</button>
+        <div className="categories">
+          <button className="box">All</button>
+          <button className="box">Active</button>
+          <button className="box">Completed</button>
+        </div>
+        <div className="todo">
+        <div className="items">
+          <input type="checkbox" />
+          <div>Item</div> 
+        </div>
+        <div className="buttons"> 
+        <button className="btn" style={{marginRight:"10px"}}>Edit</button>
+        <button className="btn">Delete</button>
+        </div>
+        <div className="items">
+          <input type="checkbox" />
+          <div>Item</div> 
+        </div>
+        <div className="buttons"> 
+        <button className="btn" style={{marginRight:"10px"}}>Edit</button>
+        <button className="btn">Delete</button>
+        </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
