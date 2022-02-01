4d1dfa31e17daef91f6d8f3e7deb731f65dc846e
rindish krishna <rindishkrishna@gmail.com>
2022-02-01 12:20:13 +0530

    Functionalities Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [text,setText]=useState("");
   const [items,setItem]=useState([
-    {"text":"Task 1"},
-    {"text":"Task 2"}
+    {"text":"Task 1", "id":uuidv4(),"active":true},
+    {"text":"Task 2","id":uuidv4(),"active":true}
   ])
+  const [activeFilter, setFilter] = useState("all");
   function addItem(txt){
     let newElement={
-      "text":txt
+      "text":txt,
+      "id":uuidv4(),
+      "active":true
     }
     setItem(oldArray => [...oldArray, newElement]);
+  }
+  function changeTaskStatus(id){
+    let item=items.map((x)=>{
+      if(x.id===id){
+        return{...x,active:!x.active}
+      }
+      return x;
+    });
+    setItem(item);
+  }
+  function deleteItem(id){
+    setItem(items.filter((x)=>x.id!=id))
+  }
+  function activeItems(){
+    setFilter("active");
+  }
+  function completedItems(){
+    setFilter("completed");
+  }
+  function allItems(){
+    setFilter("all");
   }
   return (
     <div className="container">
@@ -29,23 +53,44 @@ function App() {
         onClick={()=>addItem(text)}
         >Add</button>
         <div className="categories">
-          <button className="box">All</button>
-          <button className="box">Active</button>
-          <button className="box">Completed</button>
+        <button className="box"
+        onClick={()=>allItems(text)}        
+        >All</button>
+          <button className="box"
+            onClick={()=>activeItems()}          
+          >Active</button>
+          <button className="box"
+          onClick={()=>completedItems()} 
+          >Completed</button>
         </div>
         <div className="todo">
-          {items.map((x)=>{
+          {items
+          .filter((x)=>{
+            if(activeFilter=="all"){
+              return true;
+            }
+            else if(activeFilter=="active"){
+              return x.active;
+            }
+            else if(activeFilter=="completed"){
+              return !x.active;
+            }
+          })
+          .map((x)=>{
             return(
-              <div id={x.text}>
+              <div key={x.text}>
               <div className="items">
-              <input type="checkbox" />
+              <input type="checkbox" checked={!x.active} onClick={()=>changeTaskStatus(x.id)}/>
               <div>{x.text}</div> 
             </div>
             <div className="buttons"> 
-            <button className="btn" style={{marginRight:"10px"}}>Edit</button>
-            <button className="btn">Delete</button>
+            <button className="btn"
+            onClick={()=>deleteItem(x.id)}
+            >Delete</button>
             </div>
+            <hr/>
             </div>
+           
             )
           })}
 
